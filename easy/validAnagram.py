bdec18ca39a32d74326fff6d2f0111c084f165cc
Shams Hasan <shamshasan941@gmail.com>
2025-08-23 00:39:13 -0400

    add Valid Anagram solution

---
+++ easy/validAnagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        def checkString(s: str):   
+            freqMap = {}
+            for c in s:
+                freqMap[c] = freqMap.get(c, 0) + 1 
+            return freqMap
+            
+        return checkString(s) == checkString(t)
+
+            
+            
+
+
+
+    
+        
