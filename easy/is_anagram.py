dc67babeeba7ce1c9709a6871c3ad6559f15c16a
Shams <105460072+shamsytech@users.noreply.github.com>
2025-08-28 03:39:14 -0400

    Refactor isAnagram with example, algorithm tracing, and variable renaming for clarity

--- easy/is_anagram.py
+++ easy/is_anagram.py
@@ -1,10 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
+        Check if two strings contain equal character counts.
 
+        Example Inputs: 
+            s = "rat" 
+            t = "tar"
+
+        Algorithm Tracing (s = "rat"):
+            freq_map = {}
+            -> c = "r" -> freq_map['r'] += 1 -> {'r': 1}
+            -> c = "a" -> freq_map['a'] += 1 -> {'r': 1, 'a': 1}
+            -> c = "t" -> freq_map['t'] += 1 -> {'r': 1, 'a': 1, 't': 1}
+    
+            We return True if the frequency maps of s and t are equal.
+            -> return checkString(s) == checkString(t)
+        """
         def checkString(s: str):   
-            freqMap = {}
-            for c in s:
-                freqMap[c] = freqMap.get(c, 0) + 1 
-            return freqMap
             
+            freq_map = {}
+            for c in s:
+                freq_map[c] = freq_map.get(c, 0) + 1 
+            return freq_map
+        
         return checkString(s) == checkString(t)
