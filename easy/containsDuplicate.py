ae0d405e8a45b349962a38127cbf14914afa422c
Shams <105460072+shamsytech@users.noreply.github.com>
2025-08-28 03:14:17 -0400

    refactor hasDuplicate to include algorithm tracing and example

--- easy/containsDuplicate.py
+++ easy/containsDuplicate.py
@@ -1,10 +1,17 @@
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        '''
-        [1, 2, 3, 3]
-        seen:
-        {} -> {1} --> {1,2} --> {1,2,3} --> 3 is in seen, returns True   
-        '''
+        """
+        Check if the array contains duplicate integers.
+        
+        Example Input: [1, 2, 3, 3]
+
+        Algorithm Tracing
+            seen:
+            -> num = 1: 1 not in seen -> add 1 -> seen = {1} 
+            -> num = 2: 2 not in seen -> add 2 -> seen = {1,2}
+            -> num = 3: 3 not in seen -> add 3 -> seen = {1,2,3}
+            -> num = 3: 3 in seen: return True 
+        """
         seen = set()
 
         for num in nums:
